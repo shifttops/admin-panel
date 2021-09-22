2a98db872c8e6edb8ba9eae28f76fe3ec2936d5f
shifttops <nkupreev@biquants.com>
2021-09-22 14:30:27 +0300

    move logic from request api/filters to api/stores(with fixes 2)

--- src/helpers/filters.js
+++ src/helpers/filters.js
@@ -2,64 +2,23 @@ import { filtersRequestMapper } from "./mappers";
 
 export const createDateFilters = (filters) => {
   Object.keys(filters).forEach((filterKey) => {
+    const reqKey = filtersRequestMapper.find(
+        (item) => filterKey === item.name
+    )?.reqName;
+
+    const key = reqKey || filterKey
+
     if (
-      filterKey === "date_deployment__range" ||
-      filterKey === "date_created__range"
+        key === "date_deployment__range" ||
+        key === "date_created__range"
     ) {
-      const index = filters[filterKey].findIndex((item) => !item);
+      const index = filters[key].findIndex((item) => !item);
       if (index === 1) {
-        filters[filterKey][index] = new Date().toISOString();
+        filters[key][index] = new Date().toISOString();
       } else {
-        filters[filterKey][index] = new Date(0).toISOString();
+        filters[key][index] = new Date(0).toISOString();
       }
     }
-    // const dateFilterKey = filterKey.split("__lte")[0].split("__gte")[0];
-    // if (
-    //   filters[`${dateFilterKey}__lte`] &&
-    //   filters[`${dateFilterKey}__gte`]
-    //   ) {
-    //     filters[`${dateFilterKey}__range`] = [
-    //       filters[`${dateFilterKey}__gte`],
-    //     filters[`${dateFilterKey}__lte`],
-    //   ];
-    //   delete filters[`${dateFilterKey}__lte`];
-    //   delete filters[`${dateFilterKey}__gte`];
-
-    // } else if (filters[`${dateFilterKey}__gte`]) {
-    //   filters[`${dateFilterKey}__lte`] = new Date().toISOString();
-    // } else if (filters[`${dateFilterKey}__lte`]) {
-    //   filters[`${dateFilterKey}__gte`] = new Date().toISOString();
-    // }
-
-    // if (
-    //     filters[`${dateFilterKey}__lte`] &&
-    //     filters[`${dateFilterKey}__gte`]
-    //   ) {
-    //     filters[`${dateFilterKey}__range`] = [
-    //       filters[`${dateFilterKey}__gte`],
-    //       filters[`${dateFilterKey}__lte`],
-    //     ];
-    //     delete filters[`${dateFilterKey}__lte`];
-    //     delete filters[`${dateFilterKey}__gte`];
-
-    //   }
   });
   return filters;
-};
-
-export const configureFilters = (enabledFilters) => {
-  const filtersForReq = {};
-
-  Object.keys(enabledFilters).forEach((key) => {
-    let reqKey = filtersRequestMapper.find(
-      (item) => key === item.name
-    )?.reqName;
-    if (reqKey) {
-      filtersForReq[reqKey] = enabledFilters[key];
-    } else {
-      filtersForReq[key] = enabledFilters[key];
-    }
-  });
-
-  return createDateFilters(filtersForReq);
 };
