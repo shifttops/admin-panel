fa204193552829c06b7bb8e2e59b1371d600ac59
shifttops <nkupreev@biquants.com>
2022-01-11 17:55:55 +0300

    add button for password visibility

--- src/components/forms/InputPassword/InputPassword.jsx
+++ src/components/forms/InputPassword/InputPassword.jsx
@@ -1,15 +1,28 @@
 import styles from "components/forms/InputPassword/input-password.module.scss";
+import ButtonIcon from "../../buttons/ButtonIcon";
+import { useState } from "react";
+import Button from "../../buttons/Button";
+import cn from "classnames";
 
 export default function InputPassword({ placeholder, value, setValue }) {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
   return (
     <div className={styles.wrapper}>
       <input
         className={styles.input}
-        type="password"
+        type={!isPasswordVisible ? "password" : "text"}
         placeholder={placeholder}
-        value={value} onChange={(e) => setValue(e.target.value)}
+        value={value}
+        required
+        onChange={(e) => setValue(e.target.value)}
       />
-      <a href="#" className={styles.passwordControl}></a>
+      <div
+        onClick={() => setIsPasswordVisible((prevState) => !prevState)}
+        className={cn(styles.passwordControl, {
+          [styles.passwordControl__visible]: isPasswordVisible,
+        })}
+      />
     </div>
   );
 }
